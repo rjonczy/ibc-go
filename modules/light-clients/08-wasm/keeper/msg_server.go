a2ccabc8034fbcdd315b61d45543fbe61c01053a
rjonczy <robert.jonczy@gmail.com>
2023-11-09 12:54:29 +0100

    minor hack for devnet

--- modules/light-clients/08-wasm/keeper/msg_server.go
+++ modules/light-clients/08-wasm/keeper/msg_server.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 	"encoding/hex"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
@@ -16,9 +17,9 @@ var _ types.MsgServer = Keeper{}
 func (k Keeper) PushNewWasmCode(goCtx context.Context, msg *types.MsgPushNewWasmCode) (*types.MsgPushNewWasmCodeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	if k.authority != msg.Signer {
-		return nil, sdkerrors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority: expected %s, got %s", k.authority, msg.Signer)
-	}
+	// if k.authority != msg.Signer {
+	// 	return nil, sdkerrors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority: expected %s, got %s", k.authority, msg.Signer)
+	// }
 
 	codeID, err := k.storeWasmCode(ctx, msg.Code)
 	if err != nil {
